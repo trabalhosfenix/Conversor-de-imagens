695a1ae061425cbc5cd786141768dd2ea09025e7
fenix <trabalhos.fenix@gmail.com>
2023-01-30 23:26:58 -0300

    Add new conversors

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, Response, render_template, make_response,url_for
 from PIL import Image
 import io
+
 from io import BytesIO
 
 app = Flask(__name__)
-user = {'username': 'Miguel'}
+user = {'username': 'Visitante'}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -22,14 +23,24 @@ def pngtojpg():
 def webptopng():
     return render_template('webptopng.html', input="Webp", output="PNG", title='WEBP para PNG', user=user)
 
+@app.route("/pngtowebp", methods=["GET"])
+def pngtowebp():
+     return render_template('pngtowebp.html', input="PNG", output="WEBP", title='PNG para WEBP', user=user)
+
 @app.route('/bmptopng', methods=['GET'])
 def bmptopng():    
     return render_template('bmptopng.html', input="BMP", output="PNG", title='BMP para PNG', user=user)
+
+@app.route('/pngtobmp', methods=['GET'])
+def pngtobmp():    
+    return render_template('pngtobmp.html', input="PNG", output="BMP", title='PNG para BMP', user=user)
+
 
 @app.route('/pngtopdf', methods=['GET'])
 def pngtopdf():     
     return render_template('pngtopdf.html', input="PNG", output="PDF", title='PNG para PDF', user=user)
 
+"""  <--------------------> """
 
 @app.route("/api/pngtojpg", methods=["POST"])
 def png_to_jpg():
@@ -49,12 +60,14 @@ def jpg_to_png():
     image = request.files["image"]
     image_name = image.filename.split(".")[0] + ".png"
     image = Image.open(image)
-    image = image.convert("RGBA")
+    image = image.convert("RGB")
     byte_io=io.BytesIO()
     image.save(byte_io,"PNG" )
     byte_io.seek(0)
     response=Response(byte_io, content_type='image/png')
     response.headers.set('Content-Disposition', 'attachment', filename={image_name})
+    print(response.headers)
+   
     return response
 
 @app.route("/api/webptopng", methods=["POST"])
@@ -70,6 +83,20 @@ def webp_to_png():
     response.headers.set('Content-Disposition', 'attachment', filename={image_name})
     return response
 
+@app.route('/api/pngtowebp', methods=['POST'])
+def png_to_webp():
+    image = request.files['image']
+    image_name = image.filename.split(".")[0] + ".webp"
+    img = Image.open(image)
+    buffer = io.BytesIO()
+    img.save(buffer, format='WEBP')
+    buffer.seek(0)
+
+    response=Response(buffer, mimetype='image/webp')
+    response.headers.set('Content-Disposition', 'attachment', filename={image_name})
+
+    return response
+
 @app.route("/api/bmptopng", methods=["POST"])
 def bmp_to_png():
     image = request.files["image"]
@@ -83,8 +110,6 @@ def bmp_to_png():
     response.headers.set('Content-Disposition', 'attachment', filename={image_name})
     return response
 
-
-
 @app.route("/api/pngtopdf", methods=["POST"])
 def png_to_pdf():
     image = request.files["image"]
@@ -96,6 +121,21 @@ def png_to_pdf():
     byte_io.seek(0)
     response=Response(byte_io, content_type='application/pdf')
     response.headers.set('Content-Disposition', 'attachment', filename={image_name})
+    return response
+
+@app.route("/api/pngtobmp", methods=["POST"])
+def png_to_bmp():
+    image = request.files["image"]
+    image_name = image.filename.split(".")[0] + ".bmp"
+    image = Image.open(image)
+    image = image.convert("RGB")
+    byte_io=io.BytesIO()
+    image.save(byte_io,"BMP" )
+    byte_io.seek(0)
+    response=Response(byte_io, content_type='image/bmp')
+    response.headers.set('Content-Disposition', 'attachment', filename={image_name})
+    print(response.headers)
+   
     return response
 
 if __name__ == '__main__':
